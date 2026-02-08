95a2f90c404dde27b62ec0c6420b0a4d2a9c2aad
netoncn <netoncn@hotmail.com>
2026-02-07 22:02:22 -0300

    feat(frontend): add auth guard and http interceptors

---
+++ frontend/src/app/core/interceptors/error.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { environment } from '../../../environments/environment';
+
+export const errorInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
+
+  return next(req).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 && req.url.startsWith(environment.apiUrl)) {
+        authService.logout();
+      }
+
+      return throwError(() => error);
+    })
+  );
+};
